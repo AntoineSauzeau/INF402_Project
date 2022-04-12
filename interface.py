c0f2965fa44d39a263272547d2469aab8ae09cd7
Antoine sauzeau <antoine.sauzeau3@gmail.com>
2022-04-12 21:14:16 +0200

    12/04/2022

---
+++ interface.py
@@ -0,0 +1,7 @@
+import pygame
+
+def CreateWindow():
+    screen = pygame.display.set_mode((500, 500))
+
+def TreatEvents():
+    pass
---
+++ main.py
@@ -0,0 +1,6 @@
+import pygame
+
+black = 0, 0, 0
+
+pygame.init()
+#screen = pygame.display.set_mode((500, 500))
